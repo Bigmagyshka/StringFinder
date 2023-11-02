fd2a02a52981525144c867e1c9ae8ec4c01a2cf9
Bigmagyska <bigmagyska@gmail.com>
2023-11-02 23:38:14 +0700

    1.05
    Fix crash if Settings doesn't exist

--- MyObjects/myparserrunnable.cpp
+++ MyObjects/myparserrunnable.cpp
@@ -2,159 +2,165 @@
 
 #include <qfile.h>
 
-MyParserRunnable::MyParserRunnable(const QString &sPath, const QString &sIncludeText, const QString &sExcludeText, int nVersion,
-								   bool bUseOldStyleStream, bool bIsCaseSensetiveInclude, bool bSearchFullPhraseInclude, bool bIsCaseSensetiveExclude,
-								   bool bSearchFullPhraseExclude, QSharedPointer<bool> &bForceStop)
-	: QObject()
-	, QRunnable()
-	, m_sPath(sPath)
-	, m_bIsCaseSensetiveInclude(bIsCaseSensetiveInclude)
-	, m_bSearchFullPhraseInclude(bSearchFullPhraseInclude)
-	, m_bIsCaseSensetiveExclude(bIsCaseSensetiveExclude)
-	, m_bSearchFullPhraseExclude(bSearchFullPhraseExclude)
-	, m_nVersion(nVersion)
-	, m_bUseOldStyleStream(bUseOldStyleStream)
-	, m_bForceStop(bForceStop) {
-	m_vecIncludeText = sIncludeText.split('\n');
-	m_vecExcludeText = sExcludeText.split('\n');
+MyParserRunnable::MyParserRunnable(
+	const QString &sPath, const QString &sIncludeText,
+	const QString &sExcludeText, int nVersion, bool bUseOldStyleStream,
+	bool bIsCaseSensetiveInclude, bool bSearchFullPhraseInclude,
+	bool bIsCaseSensetiveExclude, bool bSearchFullPhraseExclude,
+	QSharedPointer<bool> &bForceStop)
+	: QObject(), QRunnable(), m_sPath(sPath),
+	  m_bIsCaseSensetiveInclude(bIsCaseSensetiveInclude),
+	  m_bSearchFullPhraseInclude(bSearchFullPhraseInclude),
+	  m_bIsCaseSensetiveExclude(bIsCaseSensetiveExclude),
+	  m_bSearchFullPhraseExclude(bSearchFullPhraseExclude),
+	  m_nVersion(nVersion), m_bUseOldStyleStream(bUseOldStyleStream),
+	  m_bForceStop(bForceStop) {
+  m_vecIncludeText = sIncludeText.split('\n');
+  m_vecExcludeText = sExcludeText.split('\n');
 
-	if(m_bSearchFullPhraseInclude) {
-		for(auto &str: m_vecIncludeText) {
-			QRegularExpression obj;
-			obj.setPattern("\\b" + str + "\\b");
-			if(!bIsCaseSensetiveInclude)
-				obj.setPatternOptions(QRegularExpression::CaseInsensitiveOption);
-			m_vecRegExpInclude.emplaceBack(obj);
-		}
+  if (m_bSearchFullPhraseInclude) {
+	for (auto &str : m_vecIncludeText) {
+	  QRegularExpression obj;
+	  obj.setPattern("\\b" + str + "\\b");
+	  if (!bIsCaseSensetiveInclude)
+		obj.setPatternOptions(QRegularExpression::CaseInsensitiveOption);
+	  m_vecRegExpInclude.emplaceBack(obj);
 	}
-	if(m_bSearchFullPhraseExclude) {
-		for(auto &str: m_vecExcludeText) {
-			QRegularExpression obj;
-			obj.setPattern("\\b" + str + "\\b");
-			if(!bIsCaseSensetiveExclude)
-				obj.setPatternOptions(QRegularExpression::CaseInsensitiveOption);
-			m_vecRegExpExclude.emplaceBack(obj);
-		}
+  }
+  if (m_bSearchFullPhraseExclude) {
+	for (auto &str : m_vecExcludeText) {
+	  QRegularExpression obj;
+	  obj.setPattern("\\b" + str + "\\b");
+	  if (!bIsCaseSensetiveExclude)
+		obj.setPatternOptions(QRegularExpression::CaseInsensitiveOption);
+	  m_vecRegExpExclude.emplaceBack(obj);
 	}
+  }
 }
 
 bool MyParserRunnable::CheckIsTextSuitable(const QString &sText) {
-	if(sText.isEmpty())
-		return false;
+  if (sText.isEmpty())
+	return false;
 
-	bool bFind {false};
-	if(m_bSearchFullPhraseInclude) {
-		for(auto &obj: m_vecRegExpInclude) {
-			if(!sText.contains(obj))
-				continue;
+  bool bFind{false};
+  if (m_bSearchFullPhraseInclude) {
+	for (auto &obj : m_vecRegExpInclude) {
+	  if (!sText.contains(obj))
+		continue;
 
-			bFind = true;
-			break;
-		}
-	} else {
-		for(auto &str: m_vecIncludeText) {
-			if(!sText.contains(str, m_bIsCaseSensetiveInclude ? Qt::CaseSensitivity::CaseSensitive : Qt::CaseSensitivity::CaseSensitive))
-				continue;
-
-			bFind = true;
-			break;
-		}
+	  bFind = true;
+	  break;
 	}
+  } else {
+	for (auto &str : m_vecIncludeText) {
+	  if (!sText.contains(str, m_bIsCaseSensetiveInclude
+								   ? Qt::CaseSensitivity::CaseSensitive
+								   : Qt::CaseSensitivity::CaseInsensitive))
+		continue;
 
-	if(!bFind)
-		return false;
-
-	bFind = false;
-	if(m_bSearchFullPhraseExclude) {
-		for(auto &obj: m_vecRegExpExclude) {
-			if(!sText.contains(obj))
-				continue;
-
-			bFind = true;
-			break;
-		}
-	} else {
-		for(auto &str: m_vecExcludeText) {
-			if(!sText.contains(str, m_bIsCaseSensetiveExclude ? Qt::CaseSensitivity::CaseSensitive : Qt::CaseSensitivity::CaseSensitive))
-				continue;
-
-			bFind = true;
-			break;
-		}
+	  bFind = true;
+	  break;
 	}
+  }
 
-	if(bFind)
-		return false;
+  if (!bFind)
+	return false;
 
-	return true;
+  bFind = false;
+  if (m_bSearchFullPhraseExclude) {
+	for (auto &obj : m_vecRegExpExclude) {
+	  if (!sText.contains(obj))
+		continue;
+
+	  bFind = true;
+	  break;
+	}
+  } else {
+	for (auto &str : m_vecExcludeText) {
+	  if (!sText.contains(str, m_bIsCaseSensetiveExclude
+								   ? Qt::CaseSensitivity::CaseSensitive
+								   : Qt::CaseSensitivity::CaseInsensitive))
+		continue;
+
+	  bFind = true;
+	  break;
+	}
+  }
+
+  if (bFind)
+	return false;
+
+  return true;
 }
 
 void MyParserRunnable::UseQByteArray() {
-	QFile file(m_sPath);
-	auto pResult = new QTreeWidgetItem(QStringList {m_sPath, "", ""});
+  QFile file(m_sPath);
+  auto pResult = new QTreeWidgetItem(QStringList{m_sPath, "", ""});
 
-	if(!file.open(QIODevice::ReadOnly)) {
-		emit SignalSendResult(pResult, m_nVersion);
-		return;
-	}
-
-	int nLine {0};
-
-	QByteArray data = file.readAll();
-	auto text {QString::fromLocal8Bit(data)};
-	data.clear();
-	auto listSplited = text.split('\n');
-	text.clear();
-
-	for(const auto &sLine: listSplited) {
-		if(*m_bForceStop)
-			break;
-
-		++nLine;
-		auto sLineText = sLine.trimmed();
-
-		if(!CheckIsTextSuitable(sLineText))
-			continue;
-
-		pResult->addChild(new QTreeWidgetItem(QStringList {"", QString::number(nLine), sLineText}));
-	}
-	file.close();
-
+  if (!file.open(QIODevice::ReadOnly)) {
 	emit SignalSendResult(pResult, m_nVersion);
+	return;
+  }
+
+  int nLine{0};
+
+  QByteArray data = file.readAll();
+  auto text{QString::fromLocal8Bit(data)};
+  data.clear();
+  auto listSplited = text.split('\n');
+  text.clear();
+
+  for (const auto &sLine : listSplited) {
+	if (*m_bForceStop)
+	  break;
+
+	++nLine;
+	auto sLineText = sLine.trimmed();
+
+	if (!CheckIsTextSuitable(sLineText))
+	  continue;
+
+	pResult->addChild(new QTreeWidgetItem(
+		QStringList{"", QString::number(nLine), sLineText}));
+  }
+  file.close();
+
+  emit SignalSendResult(pResult, m_nVersion);
 }
 
 void MyParserRunnable::UseQStream() {
-	QFile file(m_sPath);
-	auto pResult = new QTreeWidgetItem(QStringList {m_sPath, "", ""});
+  QFile file(m_sPath);
+  auto pResult = new QTreeWidgetItem(QStringList{m_sPath, "", ""});
 
-	if(!file.open(QIODevice::ReadOnly)) {
-		emit SignalSendResult(pResult, m_nVersion);
-		return;
-	}
-
-	int nLine {0};
-	QTextStream in(&file);
-
-	while(!in.atEnd()) {
-		if(*m_bForceStop)
-			break;
-
-		++nLine;
-		auto sLineText = in.readLine().trimmed();
-
-		if(!CheckIsTextSuitable(sLineText))
-			continue;
-
-		pResult->addChild(new QTreeWidgetItem(QStringList {"", QString::number(nLine), sLineText}));
-	}
-	file.close();
-
+  if (!file.open(QIODevice::ReadOnly)) {
 	emit SignalSendResult(pResult, m_nVersion);
+	return;
+  }
+
+  int nLine{0};
+  QTextStream in(&file);
+
+  while (!in.atEnd()) {
+	if (*m_bForceStop)
+	  break;
+
+	++nLine;
+	auto sLineText = in.readLine().trimmed();
+
+	if (!CheckIsTextSuitable(sLineText))
+	  continue;
+
+	pResult->addChild(new QTreeWidgetItem(
+		QStringList{"", QString::number(nLine), sLineText}));
+  }
+  file.close();
+
+  emit SignalSendResult(pResult, m_nVersion);
 }
 
 void MyParserRunnable::run() {
-	if(m_bUseOldStyleStream)
-		UseQByteArray();
-	else
-		UseQStream();
+  if (m_bUseOldStyleStream)
+	UseQByteArray();
+  else
+	UseQStream();
 }
--- filehelper.cpp
+++ filehelper.cpp
@@ -1,53 +1,56 @@
 #include "filehelper.h"
 
-namespace FileHelper
-{
-	void WriteStringToOStream(QTextStream &osStream, QPair<QString, QString> sKeyAndString){
-		osStream << sKeyAndString.first << "=" << sKeyAndString.second << Qt::endl;
-	}
-
-	bool WriteVecToFile(QString sFileName, QVector<QPair<QString, QString>> vecKeyAndString, bool bReWrite /*= true*/){
-		QFile file(sFileName);
-		if (!file.open(QIODevice::WriteOnly|(bReWrite ? QIODevice::NotOpen : QIODevice::Append)))
-			return false;
-
-		if(bReWrite)
-			file.resize(0);
-
-		QTextStream out(&file);
-		out.setEncoding(QStringConverter::Utf8);
-
-		for(auto elem : vecKeyAndString)
-			WriteStringToOStream(out, elem);
-
-		return true;
-	}
-
-	QMap<QString, QString> ReadMapFromFile(QString sFileName){
-		QFile file(sFileName);
-		if (!file.open(QIODevice::ReadOnly))
-			return {};
-
-		QTextStream in(&file);
-		in.setEncoding(QStringConverter::Utf8);
-
-		auto sTemp = in.readAll();
-		if(sTemp.isEmpty())
-			return {};
-
-		auto sReadLines = sTemp.split("\n", Qt::SkipEmptyParts);
-		sTemp.clear();
-
-		QMap<QString, QString> mapValues;
-		for(auto sLine : sReadLines){
-
-			auto sSepLine = sLine.split("=");
-			if(sSepLine.size() != 2)
-				continue;
-
-			mapValues[sSepLine[0].trimmed()] = sSepLine[1].trimmed();
-		}
-
-		return mapValues;
-	}
+namespace FileHelper {
+void WriteStringToOStream(QTextStream &osStream,
+						  QPair<QString, QString> sKeyAndString) {
+  osStream << sKeyAndString.first << "=" << sKeyAndString.second << Qt::endl;
 }
+
+bool WriteVecToFile(QString sFileName,
+					QVector<QPair<QString, QString>> vecKeyAndString,
+					bool bReWrite /*= true*/) {
+  QFile file(sFileName);
+  if (!file.open(QIODevice::WriteOnly |
+				 (bReWrite ? QIODevice::NotOpen : QIODevice::Append)))
+	return false;
+
+  if (bReWrite)
+	file.resize(0);
+
+  QTextStream out(&file);
+  out.setEncoding(QStringConverter::Utf8);
+
+  for (auto elem : vecKeyAndString)
+	WriteStringToOStream(out, elem);
+
+  return true;
+}
+
+QMap<QString, QString> ReadMapFromFile(QString sFileName) {
+  QFile file(sFileName);
+  if (!file.exists() || !file.open(QIODevice::ReadOnly))
+	return {};
+
+  QTextStream in(&file);
+  in.setEncoding(QStringConverter::Utf8);
+
+  auto sTemp = in.readAll();
+  if (sTemp.isEmpty())
+	return {};
+
+  auto sReadLines = sTemp.split("\n", Qt::SkipEmptyParts);
+  sTemp.clear();
+
+  QMap<QString, QString> mapValues;
+  for (auto sLine : sReadLines) {
+
+	auto sSepLine = sLine.split("=");
+	if (sSepLine.size() != 2)
+	  continue;
+
+	mapValues[sSepLine[0].trimmed()] = sSepLine[1].trimmed();
+  }
+
+  return mapValues;
+}
+} // namespace FileHelper
